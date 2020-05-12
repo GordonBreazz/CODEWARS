d966ff0707af1a16f0841b077866ed56e43c43c2
GordonBreazz <jc12-mail@yandex.ru>
2020-05-12 12:56:16 +0800

    complete kata "Create a regular expression capable of evaluating binary strings (which consist of only 1's and 0's) and determining whether the given string represents a number divisible by 7."

--- JS/tests/2kyu--Regular-Expression---Check-if-divisible-by-0b111-(7).test.js
+++ JS/tests/2kyu--Regular-Expression---Check-if-divisible-by-0b111-(7).test.js
@@ -1,8 +1,34 @@
 const {solution} = require('../2kyu--Regular-Expression---Check-if-divisible-by-0b111-(7).js')
-// Test.describe('Anti-cheat tests', function() {
-//     Test.assertEquals(solution instanceof RegExp, true);
-//     Test.assertEquals(solution.test.toString(), "function test() { [native code] }");
-//   });
+
+describe('Anti-cheat tests', function() {
+    test('#1', () => {
+        expect(solution instanceof RegExp).toEqual(true) 
+    })
+
+    test('#2', () => {
+        expect(solution.test.toString()).toEqual("function test() { [native code] }") 
+    })
+});
+
+describe('Edge cases', function() {
+    test('#1 You should reject empty string', () => {
+        expect(solution.test('')).toEqual(false) 
+    })
+
+    test('#2 You should accept 0', () => {
+        expect(solution.test('0')).toEqual(true) 
+    })
+});
+
+describe('Gentle fixed tests', function() {
+    test('100 fixed tests', () => {
+        for(let i=1; i<=100; i++) 
+            expect(solution.test(i.toString(2))).toEqual( i%7===0) 
+    })
+});
+
+
+
   
 //   Test.describe('Gentle fixed tests', function() {
 //     Test.it('100 fixed tests', function() {
@@ -13,7 +39,3 @@ const {solution} = require('../2kyu--Regular-Expression---Check-if-divisible-by-
 //     })
 //   })
   
-//   Test.describe('Edge cases', function() {
-//     Test.assertEquals(solution.test(''), false, 'You should reject empty string');
-//     Test.assertEquals(solution.test('0'), true, 'You should accept 0');
-//   });
