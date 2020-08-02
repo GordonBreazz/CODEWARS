59d5c2415233ef87f289b46e8d3b399747d6cad4
GordonBreazz <jc12-mail@yandex.ru>
2020-08-03 00:18:57 +0800

    doc formating

--- JS/2kyu--Break-the-pieces.js
+++ JS/2kyu--Break-the-pieces.js
@@ -1,48 +1,55 @@
-/*
-You are given a ASCII diagram , comprised of minus signs -, plus signs +, vertical bars | and whitespaces . Your task is to write a function which breaks the diagram in the minimal pieces it is made of.
+/***************************************************************************************************************************************************************************************************************************************
+* 
+*     KATA: 2 kyu "Break the pieces" 
+*   Source: https://www.codewars.com/kata/527fde8d24b9309d9b000c4e
+*   Author: https://www.codewars.com/users/GordonBreazz
+* 
+*     PLOT: You are given a ASCII diagram , comprised of minus signs -, plus signs +, vertical bars | and whitespaces . Your task is to write a function which breaks the diagram in the minimal pieces it is made of.
+*
+*   For example, if the input for your function is this diagram:
+*     
+*     +------------+
+*     |            |
+*     |            |
+*     |            |
+*     +------+-----+
+*     |      |     |
+*     |      |     |
+*     +------+-----+
+*     the returned value should be the list of:
+*     
+*     +------------+
+*     |            |
+*     |            |
+*     |            |
+*     +------------+
+*     (note how it lost a + sign in the extraction)
+*     
+*     as well as
+*     
+*     +------+
+*     |      |
+*     |      |
+*     +------+
+*     and
+*     
+*     +-----+
+*     |     |
+*     |     |
+*     +-----+
+*     The diagram is given as an ordinary Javascript multiline string. The pieces should not have trailing spaces at the end of the lines. However, it could have leading spaces if the figure is not a rectangle. For instance:
+*     
+*         +---+
+*         |   |
+*     +---+   |
+*     |       |
+*     +-------+
 
-For example, if the input for your function is this diagram:
-
-+------------+
-|            |
-|            |
-|            |
-+------+-----+
-|      |     |
-|      |     |
-+------+-----+
-the returned value should be the list of:
-
-+------------+
-|            |
-|            |
-|            |
-+------------+
-(note how it lost a + sign in the extraction)
-
-as well as
-
-+------+
-|      |
-|      |
-+------+
-and
-
-+-----+
-|     |
-|     |
-+-----+
-The diagram is given as an ordinary Javascript multiline string. The pieces should not have trailing spaces at the end of the lines. However, it could have leading spaces if the figure is not a rectangle. For instance:
-
-    +---+
-    |   |
-+---+   |
-|       |
-+-------+
-However, it is not allowed to use more leading spaces than necessary. It is to say, the first character of some of the lines should be different than a space.
-
-Finally, note that only the explicitly closed pieces are considered. Spaces "outside" of the shape are part of the background . Therefore the diagram above has a single piece.
-*/
+*   However, it is not allowed to use more leading spaces than necessary. It is to say, the first character of some of the lines should be different than a space.
+*     
+*   Finally, note that only the explicitly closed pieces are considered. Spaces "outside" of the shape are part of the background . Therefore the diagram above has a single piece.
+*     
+****************************************************************************************************************************************************************************************************************************************/
 
 function copyArray(currentArray){
   return currentArray.map( arr => arr.slice() );
--- TypeScrtipt/8kyu--Fake-Binary.ts
+++ TypeScrtipt/8kyu--Fake-Binary.ts
@@ -1,8 +1,11 @@
 /**************************************************************************************************************************************************************************************
  * 
- * 8kyu "Fake Binary"
- * 
- * Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+ *     KATA: 8kyu "Fake Binary"
+ *   Source: https://www.codewars.com/kata/57eae65a4321032ce000002d
+ *   Author: https://www.codewars.com/users/GordonBreazz 
+ *   
+ *     PLOT: Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+ *  
  * 
  **************************************************************************************************************************************************************************************/
 
