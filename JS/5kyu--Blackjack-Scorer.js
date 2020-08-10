13cf03fb1e63f0c4504e7fadb31b5dbf9808c827
GordonBreazz <jc12-mail@yandex.ru>
2020-08-11 00:13:50 +0800

    Added "Blackjack Scorer" kata and test for it

--- JS/5kyu--Blackjack-Scorer.js
+++ JS/5kyu--Blackjack-Scorer.js
@@ -1,3 +1,19 @@
+/***********************************************************************************************************************************************************
+*     KATA: 5 kyu "Blackjack Scorer"
+*   Source: https://www.codewars.com/kata/534ffb35edb1241eda0015fe
+*   Author: https://www.codewars.com/users/GordonBreazz
+*
+*     PLOT: Complete the function that determines the score of a hand in the card game Blackjack (aka 21).
+*           The function receives an array of strings that represent each card in the hand ("2", "3", ..., "10", "J", "Q", "K" or "A") 
+*           and should return the score of the hand (integer).
+*           
+*           Scoring rules:
+*             Number cards count as their face value (2 through 10). Jack, Queen and King count as 10. An Ace can be counted as either 1 or 11.
+*             Return the highest score of the cards that is less than or equal to 21. If there is no score less than or equal to 21 return 
+*             the smallest score more than 21.
+*  
+************************************************************************************************************************************************************/        
+
 function scoreHand(cards) {
     // TODO  
     let aces = 0   
@@ -14,3 +30,7 @@ function scoreHand(cards) {
     }  
     return sum  + sumAces
   }
+
+  module.exports = { scoreHand }
+
+
---
+++ JS/tests/5kyu--Blackjack-Scorer.test.js
@@ -0,0 +1,25 @@
+const { scoreHand } = require("../5kyu--Blackjack-Scorer.test")
+
+describe('"Blackjack Scorer" kata tests:', function () {
+    test("#1", () => {
+      expect(scoreHand(["2", "3"])).toEqual(5)
+    })
+    test("#2", () => {
+      expect(scoreHand(["7", "7", "8"])).toEqual(22)
+    })
+    test("#3", () => {
+      expect(scoreHand(["4", "7", "8"])).toEqual(19)
+    })
+    test("#4", () => {
+      expect(scoreHand(["K", "J", "Q"])).toEqual(30)
+    })
+    test("#5", () => {
+      expect(scoreHand(["A", "3"])).toEqual(14)
+    })
+    test("#6", () => {
+      expect(scoreHand(["A", "A"])).toEqual(12)
+    })
+    test("#7", () => {
+      expect(scoreHand(["A", "2", "A", "9", "9"])).toEqual(22)
+    })
+  })
