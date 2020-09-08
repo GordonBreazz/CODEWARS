a9aa2ef7e98328532e803a318b721056741b5a64
GordonBreazz <jc12-mail@yandex.ru>
2020-09-09 00:18:16 +0800

    fix

--- JS/5kyu--Extract-the-domain-name-from-a-URL.js
+++ JS/5kyu--Extract-the-domain-name-from-a-URL.js
@@ -5,11 +5,13 @@
 *   SOURCE: https://www.codewars.com/kata/514a024011ea4fb54200004b
 *   AUTHOR: https://www.codewars.com/users/GordonBreazz
 * 
-*     PLOT: Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+*     PLOT: Write a function that when given a URL as a string, parses out just the domain name 
+*           and returns it as a string. 
 *
-*           domainName("http://github.com/carbonfive/raygun") == "github" 
-*           domainName("http://www.zombie-bites.com") == "zombie-bites"
-*           domainName("https://www.cnet.com") == "cnet"
+*           For example:
+*             domainName("http://github.com/carbonfive/raygun") == "github" 
+*             domainName("http://www.zombie-bites.com") == "zombie-bites"
+*             domainName("https://www.cnet.com") == "cnet"
 *
 ****************************************************************************************************************************************************************************************************************************************/
 
