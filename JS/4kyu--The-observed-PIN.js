5ac39356cc08e0751db5ff654fa1191e80080053
GordonBreazz <jc12-mail@yandex.ru>
2020-09-17 23:30:31 +0800

    Added "The observed PIN" JS kata and test for it

---
+++ JS/4kyu--The-observed-PIN.js
@@ -0,0 +1,58 @@
+/***********************************************************************************************************************************************************
+*     KATA: 4 kyu "The observed PIN"
+*   Source: https://www.codewars.com/kata/5263c6999e0f40dee200059d
+*   Author: https://www.codewars.com/users/GordonBreazz
+*
+*     PLOT: Alright, detective, one of our colleagues successfully observed our target person, Robby the robber. We followed him to a secret warehouse, where 
+*           we assume to find all the stolen stuff. The door to this warehouse is secured by an electronic combination lock. Unfortunately our spy isn't sure 
+*           about the PIN he saw, when Robby entered it.
+*            
+*            The keypad has the following layout:
+*            
+*            ┌───┬───┬───┐
+*            │ 1 │ 2 │ 3 │
+*            ├───┼───┼───┤
+*            │ 4 │ 5 │ 6 │
+*            ├───┼───┼───┤
+*            │ 7 │ 8 │ 9 │
+*            └───┼───┼───┘
+*                │ 0 │
+*                └───┘
+*            He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually be another adjacent digit (horizontally or 
+*            vertically, but not diagonally). E.g. instead of the 1 it could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
+*            
+*            He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they never finally lock the system or sound the alarm. 
+*            That's why we can try out all possible (*) variations.
+*            
+*            * possible in sense of: the observed PIN itself and all variations considering the adjacent digits
+*            
+*            Can you help us to find all those variations? It would be nice to have a function, that returns an array (or a list in Java and C*) of all variations 
+*            for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs (get_pins in python, GetPINs in C*). But please note that all PINs, 
+*            the observed one and also the results, must be strings, because of potentially leading '0's. We already prepared some test cases for you.
+*            
+*            Detective, we are counting on you!
+*            
+*            For C* user: Do not use Mono. Mono is too slower when run your code.
+*
+***************************************************************************************************************************************************************************************************************************************/
+
+function around(st, digits, result){
+    if (digits.length == 0)
+      return result.push(st)
+    for (let ch of digits[0])
+      around(st + ch, digits.slice(1), result)
+    return result
+  }
+  
+  function getPINs(observed) {
+    // TODO: This is your job, detective!
+    const dic = ['08', '124', '2135', '326', '4157', '52468', '6359', '748', '80579', '968']  
+    let digits = [], result = []
+    for (let ch of String(observed))
+      digits.push( dic[Number(ch)] )
+    return around('', digits, result)    
+  }
+
+module.exports = { getPINs }  
+
+
