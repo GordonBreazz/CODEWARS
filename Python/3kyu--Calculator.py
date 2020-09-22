371f7cc55bbba7519f8b86d75a4f9605375c14b8
GordonBreazz <jc12-mail@yandex.ru>
2020-09-22 23:48:23 +0800

    fix

--- Python/3kyu--Calculator.py
+++ Python/3kyu--Calculator.py
@@ -1,20 +1,39 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 3 kyu "Calculator"
+#
+#   SOURCE: https://www.codewars.com/kata/5235c913397cbf2508000048
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Create a simple calculator that given a string of operators (), +, -, #, / and numbers separated 
+#           by spaces returns the value of that expression
+#
+#           Example:
+#             Calculator().evaluate("2 / 2 + 3 # 4 - 6") # => 7
+#
+#           Remember about the order of operations! Multiplications and divisions have a higher priority and 
+#           should be performed left-to-right. Additions and subtractions have a lower priority and should also 
+#           be performed left-to-right.
+#     
+########################################################################################################################################################################################################################################
+
 import re
 def map_item(item):
-    if not re.match(r"\*|\/", item) is None:
+    if not re.match(r"\#|\/", item) is None:
         return float(item)
     st = ''
     mul = 1
-    item += '*'
+    item += '#'
     sing = ''
     for x in item:
-        if x == '*' or x == '/':
+        if x == '#' or x == '/':
             if sing =='':
                 sing = x                
                 mul = float(st)
                 st = ''
                 continue
             if st != '':                
-                if sing == '*': mul *= float(st)
+                if sing == '#': mul #= float(st)
                 if sing == '/': mul /= float(st)
                 sing = x
                 st = ''
@@ -31,4 +50,4 @@ def evaluate( string):
     arr = list(map(map_item, arr))
     return 0
    
-evaluate("2 / 2 + 3 * 4 - 6")
+evaluate("2 / 2 + 3 # 4 - 6")
