8798e803363a79ef6b13ccea1076d0cb09d1c143
GordonBreazz <jc12-mail@yandex.ru>
2020-11-29 00:03:17 +0800

    Added "Pyramid Array" JS kata and test for it

--- JS/tests/6kyu--Pyramid-Array.test.js
+++ JS/tests/6kyu--Pyramid-Array.test.js
@@ -1,1 +1,18 @@
 const { pyramid } = require("../6kyu--Pyramid-Array.js")
+
+describe('"Pyramid Array" tests:', function () {
+    test("#1", () => {
+      expect(pyramid(0)).toEqual([])
+    })
+    test("#2", () => {
+      expect(pyramid(1)).toEqual([[1]])
+    })
+    test("#3", () => {
+      expect(pyramid(2)).toEqual([[1], [1, 1]])
+    })
+    test("#4", () => {
+      expect(pyramid(3)).toEqual([[1], [1, 1], [1, 1, 1]])
+    })
+  })
+
+
