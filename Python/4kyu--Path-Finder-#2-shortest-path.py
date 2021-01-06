58627b6359baf6e40c87d75f3eb0c56111f5ea79
GordonBreazz <jc12-mail@yandex.ru>
2021-01-06 12:10:01 +0800

    Added "Path Finder #2: shortest path" Python kata

--- Python/4kyu--Path-Finder-#2-shortest-path.py
+++ Python/4kyu--Path-Finder-#2-shortest-path.py
@@ -1,37 +1,75 @@
-def find(x, y, c, field):
-    print(x, y, field[y][x])
-    if field[y][x] != '.': return False
-    if field[y][x] == '@': return True    
-    if field[y][x] == '.': field[y][x] = str(c - 1)
-    if field[y][x + 1] == '.': field[y][x + 1] = str(c)
-    if field[y][x - 1] == '.': field[y][x - 1] = str(c)
-    if field[y + 1][x] == '.': field[y + 1][x] = str(c)
-    if field[y - 1][x] == '.': field[y - 1][x] = str(c)
-    find(x + 1, y, c + 1, field)
-    find(x - 1, y, c + 1, field)
-    find(x, y + 1, c + 1, field)
-    find(x, y - 1, c + 1, field)
-                        
+def wave_expansion(field, wave):
+    if len(wave) == 0: return False
+    new_wave = []
+    for item in wave:
+        dy, dx = item
+        c = field[dy][dx] + 1
+        if field[dy][dx + 1] == '.':
+            new_wave.append((dy, dx + 1))
+            field[dy][dx + 1] = c
+
+        if field[dy][dx - 1] == '.':
+            new_wave.append((dy, dx - 1))
+            field[dy][dx - 1] = c
+
+        if field[dy + 1][dx] == '.':
+            new_wave.append((dy + 1, dx))
+            field[dy + 1][dx] = c
+
+        if field[dy - 1][dx] == '.':
+            new_wave.append((dy - 1, dx))
+            field[dy - 1][dx] = c
+
+    wave_expansion(field, new_wave)
     return True
-    
+
 def path_finder(maze):
     if len(maze) == 1: return True
-    field = []
-    row = [' ']    
-    for c in maze:
-        if c == '\n':            
-            row.append(' ')
-            field.append(row)
-            row = [' ']
-        else:
-            row.append(c)
-    field.append(row)            
-    l = len(field[0])
-    field.insert(0, [' ' for i in range(l)])
-    field.append([' ' for i in range(l)])
-    field[l-2][l-2] = '@'
-    result = find(1, 1, 1, field)
+    field = [['W']]
+    for i in maze.split('\n'):
+        field.append(['W'] + list(i) + ['W'])
+    l = len(field[1])
+    field.append(['W' for i in range(l)])
+    field[0] = list(field[-1])
+    field[1][1] = 0
+    wave_expansion(field, [(1, 1)])
+    if field[l - 2][l - 2] == '.': return False
     for item in field:
         print(item)
-    if result == -1: return False
-    return result
+    return field[l - 2][l - 2]
+
+
+a = "\n".join([
+  ".W.",
+  ".W.",
+  "..."
+])
+
+b = "\n".join([
+  ".W.",
+  ".W.",
+  "W.."
+])
+
+c = "\n".join([
+  "......",
+  "......",
+  "......",
+  "......",
+  "......",
+  "......"
+])
+
+d = "\n".join([
+  "......",
+  "......",
+  "......",
+  "......",
+  ".....W",
+  "....W."
+])
+
+print(path_finder(a), 4)
+print(path_finder(b), False)
+print(path_finder(c), 10)
+print(path_finder(d), False)
