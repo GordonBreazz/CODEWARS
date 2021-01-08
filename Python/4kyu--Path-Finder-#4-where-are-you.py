5b43edcd9c9a27a43edfb3e163a0d5c688a1b760
GordonBreazz <jc12-mail@yandex.ru>
2021-01-08 15:27:38 +0800

    Added "Path Finder #4: where are you?" Python kata

--- Python/4kyu--Path-Finder-#4-where-are-you.py
+++ Python/4kyu--Path-Finder-#4-where-are-you.py
@@ -1,27 +1,50 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 4 kyu "Path Finder #4: where are you?"
+#
+#   SOURCE: https://www.codewars.com/kata/5a0573c446d8435b8e00009f
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Hey, Path Finder, where are you?
+#
+#           Examples:
+#                i_am_here("RLrl")      # returns [0, 0]
+#                i_am_here("r5L2l4")    # returns [4, 3]
+#                i_am_here("r5L2l4")    # returns [0, 0]
+#
+########################################################################################################################################################################################################################################
+
 def run(x, y, angle, steps):
-    dx = 0
-    dy = 0
-    if angle == 0 or angle == 360: dx = -1
-    if angle == 180: dx = 1
+    dx, dy = 0, 0       
+    if angle == 0 or angle == 360: dx = -1        
+    if angle == 180: dx = 1        
     if angle == 90: dy = 1
     if angle == 270: dy = -1        
-    for i in range(steps):
+        
+    for __ in range(steps):
         x += dx
         y += dy
+        
     return (x, y)    
 
 def i_am_here(path):    
+    # @param string "path"  is a set of actions. To solve the problem, you need to keep the current position and angle of rotation between functions call
+    # Values "l" and "r" - rotate left and right 90 degrees
+    # Values "L" and "R" - turn left and right 180 degrees    
     x, y, angle = 0, 0, 0
+    
     try:
         x, y, angle = i_am_here.pos
     except:
         i_am_here.pos =  (x, y, angle)    
-    print(path, i_am_here.pos)
+        
     if path == '': 
         i_am_here.pos =  (0, 0, 0)
         return [0, 0]
+    
     steps_st = ''
     commands = []
+    
     for ch in path:
         if ch in 'lrLR':
             if len(steps_st) > 0: 
@@ -34,29 +57,20 @@ def i_am_here(path):
         if len(steps_st) > 0:
             commands.append(int(steps_st))                
         
-    print(commands)
-    for cmd in commands:
-        if cmd == 'L': 
-            angle -= 180
-            if angle < 0: angle = 360 + angle
-            print('после поворота ', cmd, angle) 
-        elif cmd == 'R': 
-            angle += 180
-            if angle >= 360: angle = 360 - angle
-            print('после поворота ' , cmd, angle) 
-        elif cmd == 'r': 
-            angle += 90
-            if angle >= 360: angle = 360 - angle
-            print('после поворота ', cmd, angle)    
-        elif cmd == 'l': 
-            angle -= 90
-            if angle < 0: angle = 360 + angle
-            print('после поворота ', cmd, angle)     
-        else: 
-            print('выполнить столько то шагов', cmd)
-            x, y = run(x, y, angle, cmd)
-            print('run', x, y)
+    for cmd in commands:        
+        if cmd == 'L':
+            angle -= 180                            
+        elif cmd == 'R':
+            angle += 180        
+        elif cmd == 'r':
+            angle += 90                            
+        elif cmd == 'l':  
+            angle -= 90                                        
+        else:             
+            x, y = run(x, y, angle, cmd)                        
+            
+        if angle < 0: angle += 360
+        elif angle >= 360: angle -= 360
+
     i_am_here.pos = (x, y, angle)
     return [x ,y]
-    # path это набор действий. нужен указатель на текщую позицию сохраняющнй между запусками 
-    
