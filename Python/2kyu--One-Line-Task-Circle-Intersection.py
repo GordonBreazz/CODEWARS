4b5eaa99e95b8d0b78b85d8db1fcc8e16c70e5b0
GordonBreazz <jc12-mail@yandex.ru>
2021-01-09 23:54:31 +0800

    fix

---
+++ Python/2kyu--One-Line-Task-Circle-Intersection.py
@@ -0,0 +1,22 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 2 kyu "One Line Task: Circle Intersection"
+#
+#   SOURCE: https://www.codewars.com/kata/5908242330e4f567e90000a3
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Given two congruent circles a and b of radius r, return the area of their intersection rounded down to the nearest integer.
+#
+#           Code Limit
+#           Javascript: Less than 94 characters.
+#
+#           Python: Less than 128 characters.
+#
+#           Example
+#              For c1 = [0, 0], c2 = [7, 0] and r = 5,
+#
+#           the output should be 14.
+#
+########################################################################################################################################################################################################################################
+
+circleIntersection = lambda a,b,r: a + b + r # coding and coding.. shorter and more shorter..  good luck! ;-)
