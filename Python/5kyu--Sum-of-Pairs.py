c1e2b81172f18d88268c6b22dcec40af87ee863d
GordonBreazz <jc12-mail@yandex.ru>
2021-02-03 23:59:05 +0800

    Added comment for "Sum of pairs" kata

--- Python/5kyu--Sum-of-Pairs.py
+++ Python/5kyu--Sum-of-Pairs.py
@@ -1,3 +1,38 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 5 kyu "Sum of pairs"
+#
+#   SOURCE: https://www.codewars.com/kata/54d81488b981293527000c8f
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+#           
+#           sum_pairs([11, 3, 7, 5],         10)
+#                         ^--^      3 + 7 = 10
+#             == [3, 7]
+#             
+#             sum_pairs([4, 3, 2, 3, 4],         6)
+#                        ^-----^      4 + 2 = 6, indices: 0, 2 *
+#                           ^-----^      3 + 3 = 6, indices: 1, 3
+#                              ^-----^      2 + 4 = 6, indices: 2, 4
+#             entire pair is earlier, and therefore is the correct answer
+#               == [4, 2]
+#             
+#             sum_pairs([0, 0, -2, 3], 2)
+#             #  there are no pairs of values that can be added to produce 2.
+#               == None/nil/undefined (Based on the language)
+#            
+#             sum_pairs([10, 5, 2, 3, 7, 5],         10)
+#                            ^-----------^   5 + 5 = 10, indices: 1, 5
+#                                  ^--^      3 + 7 = 10, indices: 3, 4 *
+#               * entire pair is earlier, and therefore is the correct answer
+#               == [3, 7]
+#             Negative numbers and duplicate numbers can and will appear.
+#             
+#             NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements. Be sure your code doesn't time out.
+#
+########################################################################################################################################################################################################################################
+
 l1 = [1, 4, 8, 7, 3, 15]
 l2 = [1, -2, 3, 0, -6, 1]
 l3 = [20, -13, 40]
