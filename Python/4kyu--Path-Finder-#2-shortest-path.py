14cd0f207a2cdb5e2f32fd8620ecb14ab26c6dbe
GordonBreazz <jc12-mail@yandex.ru>
2021-02-07 23:53:55 +0800

    Added comment for "Path Finder #2: shortest path" Python kata

--- Python/4kyu--Path-Finder-#2-shortest-path.py
+++ Python/4kyu--Path-Finder-#2-shortest-path.py
@@ -1,3 +1,20 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 4 kyu "Path Finder #2: shortest path"
+#
+#   SOURCE: https://www.codewars.com/kata/57658bfa28ed87ecfa00058a
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: You are at position [0, 0] in maze NxN and you can only move in one of the four cardinal 
+#           directions (i.e. North, East, South, West). Return the minimal number of steps to exit 
+#           position [N-1, N-1] if it is possible to reach the exit from the starting position. 
+#            Otherwise, return false in JavaScript/Python and -1 in C++/C#/Java.
+#
+#            Empty positions are marked .. Walls are marked W. Start and exit positions are guaranteed to be empty 
+#            in all test cases.
+#
+########################################################################################################################################################################################################################################
+
 def wave_expansion(field, wave):
     if len(wave) == 0: return False
     new_wave = []
