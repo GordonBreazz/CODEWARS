27349d7525dc9e34434b6e736f4dd9e83219d2a0
GordonBreazz <jc12-mail@yandex.ru>
2021-02-12 23:59:09 +0800

    Added comment "Basic Math (Add or Subtract)" Python kata

--- Python/7kyu--Basic-Math-(Add-or-Subtract).py
+++ Python/7kyu--Basic-Math-(Add-or-Subtract).py
@@ -1,9 +1,18 @@
-# In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
-# Note: the input will not be empty.
-
-# Examples
-# "1plus2plus3plus4"  --> "10"
-# "1plus2plus3minus4" -->  "2"
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 7 kyu "Basic Math (Add or Subtract)"
+#
+#   SOURCE: https://www.codewars.com/kata/5809b62808ad92e31b000031
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+#           Note: the input will not be empty.
+#
+#           Examples
+#             "1plus2plus3plus4"  --> "10"
+#             "1plus2plus3minus4" -->  "2"
+#
+########################################################################################################################################################################################################################################
 
 def calculate(s):
     # your code here
