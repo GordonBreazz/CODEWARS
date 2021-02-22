0cccfae20ef54fa00a0a3eb248b42dc8613ead8e
GordonBreazz <jc12-mail@yandex.ru>
2021-02-22 23:59:12 +0800

    fix

--- Python/7kyu--Will-you-make-it.py
+++ Python/7kyu--Will-you-make-it.py
@@ -1,5 +1,20 @@
-# Will you make it?
-# You were camping with your friends far away from home, but when it's time to go back, you realize that you fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog) if it is possible and false (0 in Prolog) if not. The input values are always positive.
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 7 kyu "Will you make it?"
+#
+#   SOURCE: https://www.codewars.com/kata/5861d28f124b35723e00005e
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Will you make it?
+#           You were camping with your friends far away from home, but when it's time to go back, 
+#           you realize that you fuel is running out and the nearest pump is 50 miles away! 
+#           You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. 
+#           Considering these factors, write a function that tells you if it is possible to get to the pump or not. 
+#
+#           Function should return true (1 in Prolog) if it is possible and false (0 in Prolog) if not. 
+#           The input values are always positive.
+#     
+########################################################################################################################################################################################################################################
 
 def zero_fuel(distance_to_pump, mpg, fuel_left):
     return mpg * fuel_left >= distance_to_pump
