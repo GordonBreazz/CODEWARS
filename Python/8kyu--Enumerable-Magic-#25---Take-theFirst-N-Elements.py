8cc654d27092ca0fb33f90dd3e132a961b99ebf2
GordonBreazz <jc12-mail@yandex.ru>
2021-02-23 23:30:57 +0800

    fix

--- Python/8kyu--Enumerable-Magic-#25---Take-theFirst-N-Elements.py
+++ Python/8kyu--Enumerable-Magic-#25---Take-theFirst-N-Elements.py
@@ -5,8 +5,8 @@
 #   SOURCE: https://www.codewars.com/kata/545afd0761aa4c3055001386
 #   AUTHOR: https://www.codewars.com/users/GordonBreazz
 # 
-#     PLOT: Create a method take that accepts a list/array and a number n, and returns a list/array array of the first 
-#           n elements from the list/array.
+#     PLOT: Create a method take that accepts a list/array and a number n, and returns a list/array array of 
+#           the first n elements from the list/array.
 #     
 ########################################################################################################################################################################################################################################
 
