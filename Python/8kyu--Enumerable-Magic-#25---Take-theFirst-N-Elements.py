fca6730c2cd160c9fac991902a7b56a2945da04f
GordonBreazz <jc12-mail@yandex.ru>
2021-02-23 23:30:17 +0800

    ашч

--- Python/8kyu--Enumerable-Magic-#25---Take-theFirst-N-Elements.py
+++ Python/8kyu--Enumerable-Magic-#25---Take-theFirst-N-Elements.py
@@ -1,4 +1,14 @@
-# Create a method take that accepts a list/array and a number n, and returns a list/array array of the first n elements from the list/array.
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 8 kyu "Enumerable Magic #25 - Take the First N Elements"
+#
+#   SOURCE: https://www.codewars.com/kata/545afd0761aa4c3055001386
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Create a method take that accepts a list/array and a number n, and returns a list/array array of the first 
+#           n elements from the list/array.
+#     
+########################################################################################################################################################################################################################################
 
 def take(arr,n):
     return arr[:n]
