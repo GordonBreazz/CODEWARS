2d17e09448de3a435c345c21d0bdda128f7706df
GordonBreazz <jc12-mail@yandex.ru>
2021-02-24 23:54:35 +0800

    fix

--- Python/8kyu--Get-Planet-Name-By-ID.py
+++ Python/8kyu--Get-Planet-Name-By-ID.py
@@ -1,5 +1,14 @@
-# The function is not returning the correct values. Can you figure out why?
-# get_planet_name(3) # should return 'Earth'
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 8 kyu "Get Planet Name By ID"
+#
+#   SOURCE: https://www.codewars.com/kata/515e188a311df01cba000003
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: The function is not returning the correct values. Can you figure out why?
+#           get_planet_name(3) # should return 'Earth'
+#     
+########################################################################################################################################################################################################################################
 
 def get_planet_name(id):
     # This doesn't work; Fix it!
