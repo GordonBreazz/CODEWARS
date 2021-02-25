346bb168e85e945fe7c1dcbb124f44073b2fe19c
GordonBreazz <jc12-mail@yandex.ru>
2021-02-25 23:53:02 +0800

    fix

--- Python/2kyu--Break-the-pieces.py
+++ Python/2kyu--Break-the-pieces.py
@@ -1,3 +1,63 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 2 kyu "Break the pieces" 
+#
+#   SOURCE: https://www.codewars.com/kata/527fde8d24b9309d9b000c4e
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+# 
+#     PLOT: You are given a ASCII diagram , comprised of minus signs -, plus signs +, vertical bars | and whitespaces. 
+#           Your task is to write a function which breaks the diagram in the minimal pieces it is made of.
+#
+#   For example, if the input for your function is this diagram:
+#     
+#     +------------+
+#     |            |
+#     |            |
+#     |            |
+#     +------+-----+
+#     |      |     |
+#     |      |     |
+#     +------+-----+
+#     the returned value should be the list of:
+#     
+#     +------------+
+#     |            |
+#     |            |
+#     |            |
+#     +------------+
+#     (note how it lost a + sign in the extraction)
+#     
+#     as well as
+#     
+#     +------+
+#     |      |
+#     |      |
+#     +------+
+#     and
+#     
+#     +-----+
+#     |     |
+#     |     |
+#     +-----+
+#     The diagram is given as an ordinary Javascript multiline string. The pieces should not have trailing spaces at the end of the lines. However, 
+#     it could have leading spaces if the figure is not a rectangle. 
+#
+#     For instance:
+#     
+#         +---+
+#         |   |
+#     +---+   |
+#     |       |
+#     +-------+
+
+#   However, it is not allowed to use more leading spaces than necessary. It is to say, the first character of some of the lines should be different than a space.
+#     
+#   Finally, note that only the explicitly closed pieces are considered. Spaces "outside" of the shape are part of the background. 
+#   Therefore the diagram above has a single piece.
+#     
+########################################################################################################################################################################################################################################
+
 USE_BREAK_DISPLAY = True
 import copy
 def copyArray(currentArray):
