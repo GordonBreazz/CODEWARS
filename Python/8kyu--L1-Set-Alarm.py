0bb40eb200c2a972d964b4b2a956f1e5ca9711b5
GordonBreazz <jc12-mail@yandex.ru>
2021-03-08 23:54:20 +0800

    new kata

---
+++ Python/8kyu--L1-Set-Alarm.py
@@ -0,0 +1,24 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 3 kyu "Binomial Expansion"
+#
+#   SOURCE: https://www.codewars.com/kata/540d0fdd3b6532e5c3000b5b
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Write a function named setAlarm which receives two parameters. The first parameter, employed, 
+#           is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+#           The function should return true if you are employed and not on vacation (because these are 
+#           the circumstances under which you need to set an alarm). It should return false otherwise. 
+# 
+#           Examples:
+#             setAlarm(true, true) -> false
+#             setAlarm(false, true) -> false
+#             setAlarm(false, false) -> false
+#             setAlarm(true, false) -> true
+#
+########################################################################################################################################################################################################################################
+
+def set_alarm(employed, vacation):
+    # Your code here
+    if employed and not vacation: return True
+    return False
