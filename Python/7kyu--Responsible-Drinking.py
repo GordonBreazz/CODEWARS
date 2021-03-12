93af39be971e62c3a4fb56c9876b07aaa1cec145
GordonBreazz <jc12-mail@yandex.ru>
2021-03-12 23:58:42 +0800

    fix

--- Python/7kyu--Responsible-Drinking.py
+++ Python/7kyu--Responsible-Drinking.py
@@ -13,13 +13,13 @@
 #           Return a string suggesting how many glasses of water you should drink to not be hungover.
 #
 #           Examples
-#             "1 beer"  -->  "1 glass of water"
-#             because you drank one standard drink
+#              "1 beer"  -->  "1 glass of water"
+#               because you drank one standard drink
 #
-#             "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
-#             because you drank ten standard drinks
+#               "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+#               because you drank ten standard drinks
 #           Note:
-#             To keep the things simple, we'll consider that any "numbered thing" in the string is a drink. Even "1 bear" -> "1 glass of water"; or "1 chainsaw and 2 pools" -> "3 glasses of water"...
+#               To keep the things simple, we'll consider that any "numbered thing" in the string is a drink. Even "1 bear" -> "1 glass of water"; or "1 chainsaw and 2 pools" -> "3 glasses of water"...
 #     
 ########################################################################################################################################################################################################################################
 
