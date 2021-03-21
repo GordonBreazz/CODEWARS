87458ef8af12e68cceba19e3e100413cc18b0c00
GordonBreazz <jc12-mail@yandex.ru>
2021-03-21 23:38:26 +0800

    fix

--- Python/yandex.py
+++ Python/yandex.py
@@ -1,14 +1,14 @@
-f = open("input.txt", "r")
-fo = open("output.txt", "w")
+# f = open("input.txt", "r")
+# fo = open("output.txt", "w")
 
-digits = set(map(int, f.readline().split(' ')))
-count = f.readline()
-data = []
-for x in f:
-  data = set(map(int, x.split(' ')))  
-  fo.write('Lucky\n') if len(data & digits) >= 3 else fo.write('Unlucky\n')
-fo.close()
-f.close()
+# digits = set(map(int, f.readline().split(' ')))
+# count = f.readline()
+# data = []
+# for x in f:
+#   data = set(map(int, x.split(' ')))  
+#   fo.write('Lucky\n') if len(data & digits) >= 3 else fo.write('Unlucky\n')
+# fo.close()
+# f.close()
 
 
 
