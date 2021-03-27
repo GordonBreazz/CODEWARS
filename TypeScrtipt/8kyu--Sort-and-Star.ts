0b94ef867f69154dbc62c4fc02c8db35c71f38db
GordonBreazz <jc12-mail@yandex.ru>
2021-03-28 00:02:18 +0800

    Added "Sort and Star" Typescript kata

--- TypeScrtipt/8kyu--Sort-and-Star.ts
+++ TypeScrtipt/8kyu--Sort-and-Star.ts
@@ -5,11 +5,12 @@
 *   SOURCE: https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/
 *   AUTHOR: https://www.codewars.com/users/GordonBreazz
 *
-*     PLOT: You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+*     PLOT: You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on 
+*           the ASCII values of the chars) and then return the first value.
+*
 *           The returned value must be a string, and have "***" between each of its letters.
+*
 *           You should not remove or add elements from/to the array.
-*
-*
 *
 ****************************************************************************************************************************************************************************************************************************************/
 
