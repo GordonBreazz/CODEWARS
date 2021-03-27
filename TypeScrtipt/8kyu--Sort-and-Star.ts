c4e5ee5c64910bc4e4a355ba8582cdc1c9d12410
GordonBreazz <jc12-mail@yandex.ru>
2021-03-27 23:59:54 +0800

    fix

---
+++ TypeScrtipt/8kyu--Sort-and-Star.ts
@@ -0,0 +1,19 @@
+/***************************************************************************************************************************************************************************************************************************************
+*
+*     KATA: 8 kyu "Sort and Star"
+*
+*   SOURCE: https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/
+*   AUTHOR: https://www.codewars.com/users/GordonBreazz
+*
+*     PLOT: You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+*           The returned value must be a string, and have "***" between each of its letters.
+*           You should not remove or add elements from/to the array.
+*
+*
+*
+****************************************************************************************************************************************************************************************************************************************/
+
+
+export function twoSort(s: string[]): string {
+    return string(s.sort()[0].split().join('***'))
+  }
