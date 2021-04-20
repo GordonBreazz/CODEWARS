fd86a092713138efee5228a4326dfce4bc7533d0
GordonBreazz <jc12-mail@yandex.ru>
2021-04-21 00:22:53 +0800

    Added "Get Nth Even Number" Golang kata

--- Go/8kyu--Get-Nth-Even-Number.go
+++ Go/8kyu--Get-Nth-Even-Number.go
@@ -1,5 +1,25 @@
+/***************************************************************************************************************************************************************************************************************************************
+ *
+ *     KATA: 8 kyu "Get Nth Even Number"
+ *
+ *   SOURCE: https://www.codewars.com/kata/5933a1f8552bc2750a0000ed
+ *   AUTHOR: https://www.codewars.com/users/GordonBreazz
+ *
+ *     PLOT: Return the Nth Even Number
+ *
+ *           Example(Input --> Output)
+ *
+ *           1 --> 0 (the first even number is 0)
+ *           3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+ *           100 --> 198
+ *           1298734 --> 2597466
+ *           The input will not be 0.
+ *
+ ************************************************************************************************************************************************************/
+
 package kata
 
 func NthEven(n int) int {
 	// your code here
+	return n*2 - 2
 }
