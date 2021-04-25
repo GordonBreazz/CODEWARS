6c730925783d83b3741a17947f90cc120785897c
GordonBreazz <jc12-mail@yandex.ru>
2021-04-26 00:13:42 +0800

    Added "Moves in squared strings (I)" golang kate

--- Go/7kyu--Moves-in-squared-strings(I).go
+++ Go/7kyu--Moves-in-squared-strings(I).go
@@ -1,24 +1,79 @@
-package kata
+/***************************************************************************************************************************************************************************************************************************************
+*
+*     KATA: 7 kyu "Moves in squared strings (I)"
+*
+*   SOURCE: https://www.codewars.com/kata/56dbe0e313c2f63be4000b25
+*   AUTHOR: https://www.codewars.com/users/GordonBreazz
+*
+*
+*     PLOT: This kata is the first of a sequence of four about "Squared Strings".
+*
+*           You are given a string of n lines, each substring being n characters long: For example:
+*
+*           s = "abcd\nefgh\nijkl\nmnop"
+*
+*           We will study some transformations of this square of strings.
+*
+*           Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+*           vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+*           Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+*           hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+*           or printed:
+*
+*           vertical mirror   |horizontal mirror
+*           abcd --> dcba     |abcd --> mnop
+*           efgh     hgfe     |efgh     ijkl
+*           ijkl     lkji     |ijkl     efgh
+*           mnop     ponm     |mnop     abcd
+*           Task:
+*           Write these two functions
+*           and
+*
+*           high-order function oper(fct, s) where
+*
+*           fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+*
+*           Examples:
+*           s = "abcd\nefgh\nijkl\nmnop"
+*           oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+*           oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+*           Note:
+*           The form of the parameter fct in oper changes according to the language. You can see each form according
+*           to the language in "Sample Tests".
+*
+*           Bash Note:
+*           The input strings are separated by , instead of \n. The output strings should be separated by \r instead
+*           of \n. See "Sample Tests".
+*
+****************************************************************************************************************************************************************************************************************************************/
 
-import "fmt"
+package kata
 
 func VertMirror(s string) string {
 	// your code
 	var result, st string
-	fmt.Println(s)
 	for i := 0; i < len(s); i++ {
 		if s[i] == '\n' {
-			result += st
+			result += st + "\n"
+			st = ""
 		} else {
-			st = string(s[i])
+			st = string(s[i]) + st
 		}
-		fmt.Println(string(s[i]))
 	}
-	return result
+	return result + st
 }
 func HorMirror(s string) string {
 	// your code
-	return ""
+	var result, st string
+	for i := 0; i < len(s); i++ {
+		if s[i] == '\n' {
+			result = "\n" + st + result
+			st = ""
+		} else {
+			st += string(s[i])
+		}
+	}
+	return st + result
 }
 
 type FParam func(string) string
