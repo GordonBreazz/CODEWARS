7b0b389f1071d5ec039a08045068322f63ca97ef
GordonBreazz <jc12-mail@yandex.ru>
2021-04-30 23:57:36 +0800

    fix

---
+++ Python/2kyu--Evaluate-mathematical-expression.py
@@ -0,0 +1,36 @@
+import re
+def map_item(item):
+    if not re.match(r"\#|\/", item) is None:
+        return float(item)
+    st = ''
+    mul = 1
+    item += '#'
+    sing = ''
+    for x in item:
+        if x == '#' or x == '/':
+            if sing =='':
+                sing = x                
+                mul = float(st)
+                st = ''
+                continue
+            if st != '':                
+                if sing == '#': mul #= float(st)
+                if sing == '/': mul /= float(st)
+                sing = x
+                st = ''
+        else:
+            st += x
+    return mul        
+
+def evaluate(string):
+    if re.match(r"^\d+$", string): return float(string)
+    string = re.sub(r"\s", "", string)
+    string = re.sub(r"\-", "+-", string)    
+    string += '+0'
+    arr = string.split("+")
+    arr = list(map(map_item, arr))
+    return 0
+
+def calc(expression):
+    print(evaluate(expression))
+    return # evaluated expression
