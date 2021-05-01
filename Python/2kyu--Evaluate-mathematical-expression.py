f1082799568d9c386736f2d8d67be3e95597e800
GordonBreazz <jc12-mail@yandex.ru>
2021-05-01 23:41:56 +0800

    fix

--- Python/2kyu--Evaluate-mathematical-expression.py
+++ Python/2kyu--Evaluate-mathematical-expression.py
@@ -1,36 +1,61 @@
 import re
+
+
 def map_item(item):
-    if not re.match(r"\#|\/", item) is None:
+    if not re.match(r"\*|\/", item) is None:
         return float(item)
-    st = ''
+    st, sing = '', ''
     mul = 1
-    item += '#'
-    sing = ''
+    item += '*'
     for x in item:
-        if x == '#' or x == '/':
-            if sing =='':
-                sing = x                
+        if x == '*' or x == '/':
+            if sing == '':
+                sing = x
                 mul = float(st)
                 st = ''
                 continue
-            if st != '':                
-                if sing == '#': mul #= float(st)
+            if st != '':
+                if sing == '*': mul *= float(st)
                 if sing == '/': mul /= float(st)
                 sing = x
                 st = ''
         else:
             st += x
-    return mul        
+    return mul
 
-def evaluate(string):
-    if re.match(r"^\d+$", string): return float(string)
+
+def eva(string):
+    print(string)
     string = re.sub(r"\s", "", string)
-    string = re.sub(r"\-", "+-", string)    
-    string += '+0'
+    if re.match(r"^\-?\d+$", string): return float(string)
+    string = re.sub(r"\-\-", "+", string)
+    string = re.sub(r"\-", "+-", string)
+    string = re.sub(r"\+\+", "+", string)
+    print(string)
     arr = string.split("+")
+    print(arr)
     arr = list(map(map_item, arr))
-    return 0
+    return sum(arr)
+
 
 def calc(expression):
-    print(evaluate(expression))
-    return # evaluated expression
+    s = ''
+    s1 = ''
+    a = 0
+    for ch in expression:
+        print('s:',s)
+        if ch == '(':
+            a += 1
+            s1 = ''
+            continue
+        if ch == ')':
+            a -= 1
+            if a == 0: s += str(eva(s1))
+            continue
+        if a == 0:
+            s += ch
+        else:
+            s1 += ch
+    print('ok', s)
+    st = eva(s)
+    return st  # evaluated expression
