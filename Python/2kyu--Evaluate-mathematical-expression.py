3173369ff42ab28d46051e6d7a81049c4822fc44
GordonBreazz <jc12-mail@yandex.ru>
2021-05-02 11:44:57 +0800

    Added "Evaluate mathematical expression" Python kata

--- Python/2kyu--Evaluate-mathematical-expression.py
+++ Python/2kyu--Evaluate-mathematical-expression.py
@@ -1,8 +1,63 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 2 kyu "Evaluate mathematical expression"
+#
+#   SOURCE: https://www.codewars.com/kata/52a78825cdfc2cfc87000005
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+#     PLOT: Instructions
+#           Given a mathematical expression as a string you must return the result as a number.
+#           
+#           Numbers
+#           Number may be both whole numbers and/or decimal numbers. The same goes for the returned result.
+#           
+#           Operators
+#           You need to support the following mathematical operators:
+#           
+#             Multiplication *
+#             Division / (as floating point division)
+#             Addition +
+#             Subtraction -
+#             Operators are always evaluated from left-to-right, and * and / must be evaluated before + and -.
+#           
+#           Parentheses
+#           You need to support multiple levels of nested parentheses, ex. (2 / (2 + 3.33) * 4) - -6
+#           
+#           Whitespace
+#           There may or may not be whitespace between numbers and operators.
+#           
+#           An addition to this rule is that the minus sign (-) used for negating numbers and parentheses will never be separated by whitespace. I.e all of the following are valid expressions.
+#           
+#             1-1    // 0
+#             1 -1   // 0
+#             1- 1   // 0
+#             1 - 1  // 0
+#             1- -1  // 2
+#             1 - -1 // 2
+#           
+#             6 + -(4)   // 2
+#             6 + -( -4) // 10
+#
+#           And the following are invalid expressions
+#           
+#             1 - - 1    // Invalid
+#             1- - 1     // Invalid
+#             6 + - (4)  // Invalid
+#             6 + -(- 4) // Invalid
+#
+#           Validation
+#           You do not need to worry about validation - you will only receive valid mathematical expressions following the above rules.
+#           
+#           Restricted APIs
+#           NOTE: eval and exec are disallowed in your solution.
+#                    
+########################################################################################################################################################################################################################################
+
 import re
 
-
 def map_item(item):
-    if not re.match(r"\*|\/", item) is None:
+    if item == '':
+        return 0
+    if not re.search(r"\*|\/", item):
         return float(item)
     st, sing = '', ''
     mul = 1
@@ -24,38 +79,38 @@ def map_item(item):
     return mul
 
 
-def eva(string):
-    print(string)
+def eva(string):    
     string = re.sub(r"\s", "", string)
     if re.match(r"^\-?\d+$", string): return float(string)
     string = re.sub(r"\-\-", "+", string)
     string = re.sub(r"\-", "+-", string)
     string = re.sub(r"\+\+", "+", string)
-    print(string)
-    arr = string.split("+")
-    print(arr)
+    string = re.sub(r"\*\+\-", "*-", string)
+    string = re.sub(r"\/\+\-", "/-", string)
+    string = re.sub(r"\*\+", "*", string)
+    string = re.sub(r"\/\+", "/", string)    
+    arr = string.split("+")    
     arr = list(map(map_item, arr))
     return sum(arr)
 
 
 def calc(expression):
-    s = ''
+    expr_st = ''
     s1 = ''
     a = 0
     for ch in expression:
-        print('s:',s)
-        if ch == '(':
-            a += 1
-            s1 = ''
-            continue
         if ch == ')':
             a -= 1
-            if a == 0: s += str(eva(s1))
-            continue
+            if a == 0:
+                expr_st += str(calc(s1))
+                continue
+        s1 += ch
+        if ch == '(':
+            if a == 0:
+                s1 = ''
+            a += 1
+
         if a == 0:
-            s += ch
-        else:
-            s1 += ch
-    print('ok', s)
-    st = eva(s)
+            expr_st += ch
+    st = eva(expr_st)
     return st  # evaluated expression
