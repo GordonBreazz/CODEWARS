6c9deb802cfb240be3aa88f7b8b91aad4f55c03a
GordonBreazz <jc12-mail@yandex.ru>
2021-05-04 00:06:34 +0800

    Added "Valid Spacing" Python kata

--- Python/7kyu--Valid-Spacing.py
+++ Python/7kyu--Valid-Spacing.py
@@ -1,5 +1,31 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 7 kyu "Valid Spacing"
+#
+#   SOURCE: https://www.codewars.com/kata/5f77d62851f6bc0033616bd8
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either True or False.
+#           
+#           For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Below are some examples of what the function should return.
+#           
+#           'Hello world' = True
+#           ' Hello world' = False
+#           'Hello world  ' = False
+#           'Hello  world' = False
+#           'Hello' = True
+#
+#           Even though there are no spaces, it is still valid because none are needed
+#           True because we are not checking for the validity of words.
+#
+#           'Helloworld ' = False
+#           ' ' = False
+#           '' = True
+#
+#######################################################################################################################################################################################################################################
+
 def valid_spacing(s):
     # write your code here
-    if s[0] == ' ' or s[-1] == ' ': return False
-    if s.find('  '): return False
+    if len(s) > 0:
+        if s[0] == ' ' or s[-1] == ' ' or s.find('  ') != -1: return False
     return True
