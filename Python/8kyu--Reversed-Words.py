8df9a06f2de9fe4a0eef28a9f6de66392ef55e5e
GordonBreazz <jc12-mail@yandex.ru>
2021-05-06 00:09:18 +0800

    Added "Reversed Words" Python kata

--- Python/8kyu--Reversed-Words.py
+++ Python/8kyu--Reversed-Words.py
@@ -1,6 +1,20 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 8 kyu "Reversed Words"
+#
+#   SOURCE: https://www.codewars.com/kata/51c8991dee245d7ddf00000e
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: Complete the solution so that it reverses all of the words within the string passed in.
+#           
+#           Example:
+#           
+#           "The greatest victory is that which requires no battle" --> "battle no requires which that is victory 
+#           greatest The"
+#
+#######################################################################################################################################################################################################################################
+           
 def reverse_words(s):
-    result = ''
-    for item in s.split(' '):
-        print(item.reverse())
-        result += ' '#.join()
-    return result
+    result = s.split(' ')
+    result.reverse()
+    return ' '.join(result)
