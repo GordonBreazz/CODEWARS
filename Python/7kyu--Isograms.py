2e144d9184166f0358bf5e50dd4f31e47227f440
GordonBreazz <jc12-mail@yandex.ru>
2021-05-26 00:12:37 +0800

    Added "Isograms" Python kata

--- Python/7kyu--Isograms.py
+++ Python/7kyu--Isograms.py
@@ -1,2 +1,22 @@
+#######################################################################################################################################################################################################################################
+# 
+#     KATA: 7 kyu "Isograms"
+#
+#   SOURCE: https://www.codewars.com/kata/54ba84be607a92aa900000f1
+#   AUTHOR: https://www.codewars.com/users/GordonBreazz
+# 
+#     PLOT: An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+#           
+#           is_isogram("Dermatoglyphics" ) == true
+#           is_isogram("aba" ) == false
+#           is_isogram("moOse" ) == false # -- ignore letter case
+#
+########################################################################################################################################################################################################################################
+
 def is_isogram(string):
-    #your code here
+    dic = {chr(i+65):1 for i in range(26)}
+    for ch in string:
+        dic[ch.upper()] = dic[ch.upper()] - 1
+        if dic[ch.upper()] < 0:
+            return False
+    return True
