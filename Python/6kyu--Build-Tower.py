404e381e655c4dba8a11fa52767abcc79d9c053c
GordonBreazz <jc12-mail@yandex.ru>
2021-05-31 23:56:26 +0800

    fix

--- Python/6kyu--Build-Tower.py
+++ Python/6kyu--Build-Tower.py
@@ -1,8 +1,15 @@
+def func(n, l):
+    st = '*' * n
+    space = (l-n) // 2
+    while len(st) < l:
+        st = ' ' * space + st + ' ' * space
+    return st
+
 def tower_builder(n_floors):
-    result = ['*' * n_floors,' * ']
-    for i in n_flours-1:
-        st = result[0]
-        
-        result = [result[0][2:-1]] + result
-    return result[::-1]
+    result = []
+    p = 1
+    for i in range(n_floors):        
+        result.append(func(p, n_floors))        
+        p += 2
+    return result
     # build here
