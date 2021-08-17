73c7092242f1b1589a43d9c70e83c2e69597041b
GordonBreazz <jc12-mail@yandex.ru>
2021-08-18 00:05:01 +0800

    Added "Multiples of 3 or 5" JS kata and test for it

---
+++ JS/tests/6kyu--Multiples-of-3-or-5.test.js
@@ -0,0 +1,8 @@
+const { solution } = require("../6kyu--Multiples-of-3-or-5.js")
+
+describe('Tests for "Multiples of 3 or 5" kata:', function () {
+    test("#1", () => {
+        expect(solution(10)).toEqual(23)
+    })
+  }
+)    
